d2fbf76de41f2196e4f4f530226655d9079dfb17
Numan Can İpekçi <n.canipekci@gmail.com>
2019-06-02 15:29:29 +0300

    Update Queen8GA.py

--- Queen8GA.py
+++ Queen8GA.py
@@ -1,3 +1,9 @@
+# The eight queens puzzle is the problem of
+# placing eight chess queens on an 8×8 chessboard
+# so that no two queens threaten each other; thus,
+# a solution requires that no two queens share the
+# same row, column, or diagonal.
+
 import numpy as np
 import random
 import time
